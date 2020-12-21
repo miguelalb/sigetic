92a58705bbc6453bfdab1144bef6c8db198eec01
Miguel Acevedo <miguelacevedodrv@users.noreply.github.com>
2020-12-21 15:12:00 -0400

    fixed service and domain

---
+++ ProyectosDGTIC/ClientApp/src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+    
+
+export default {
+  name: 'App',
+  components: {
+     
+  }
+}
+</script>
+
+<style>
+
+/* GENERIC STYLES */
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;500;600;700;800&display=swap');
+:root {
+  --primary-color: #05549B;
+  --secondary-color: #EC2329;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Monserrat', sans-serif;
+  line-height: 1.6;
+  background-image: url('./assets/img/dgtic-bg.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: bottom;
+  background-size: cover;
+}
+
+.container {
+  margin: 0 20px;
+}
+
+
+
+/* BUTTONS */
+.btn {
+  cursor: pointer;
+  display: inline-block;
+  padding: 10px 30px;
+  border-radius: 30px;
+  color: #fff;
+  border: none;
+  background: var(--primary-color);
+} 
+.btn:focus { outline:0; }
+
+.btn-light {
+  color: var(--primary-color);
+  background: #fff;
+  border: 2px solid var(--primary-color);
+}
+.btn-light:hover {
+  color: #fff;
+  border: none;
+  background: var(--primary-color);
+}
+
+/* FORM ELEMENTS */
+form {
+  /* width: 100%; */
+  height: 100%;
+  width: 22rem;
+  margin: 0 auto;
+}
+.form-group {
+  width: 100%;
+}
+
+/* CARDS */
+.card {
+    background-color: #fff;
+    height: 34rem;
+    max-width: 35rem;
+    margin: 4rem auto;
+    border-radius: 25px;
+    padding: 2rem;
+    box-shadow: 0 10px 30px rgba(0, 0, 0, 0.5);
+}
+
+/* CARD-FORM */
+.card form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 1px;
+}
+.card form h2 {
+  font-weight: 700;
+  margin-bottom: 1rem;
+  margin-top: 1rem;
+  font-size: 1.9rem;
+  font-family: "Montserrat", sans-serif;
+}
+</style>
---
+++ ProyectosDGTIC/ClientApp/src/components/Register.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="register">
+    <NavMenu page="Login" />
+    <div class="card">
+      <form>
+        <h2 class="form-title">REGISTER</h2>
+        <div class="form-group">
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="&#61447;"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="&#xf0e0;"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            id="password"
+            name="username"
+            placeholder="&#61475;"
+          />
+        </div>
+        <button class="btn btn-light btn-register">SIGN UP</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavMenu from "./NavMenu.vue";
+
+export default {
+  name: "Register",
+  components: {
+    NavMenu,
+  },
+};
+</script>
+
+<style scoped>
+@import "//netdna.bootstrapcdn.com/font-awesome/3.0/css/font-awesome.css";
+/* INPUTS */
+
+form input {
+  display: block;
+  width: 100%;
+  border-radius: 30px;
+  margin: 10px 0;
+  height: 3.54rem;
+  background-color: rgba(196, 196, 196, 0.54);
+  border: none;
+  font-size: 2rem;
+  font-weight: 100;
+}
+/* REMOVE OUTLINE */
+form input:focus {
+  outline: none;
+  text-indent: 20px;
+}
+/* REMOVE PLACEHOLDER ON FOCUS */
+form input:focus::placeholder{
+  color: transparent;
+}
+/* FONT-AWESOME PLACEHOLDER */
+form input::-webkit-input-placeholder {
+  font-family: FontAwesome;
+  font-weight: normal;
+  overflow: visible;
+  vertical-align: top;
+  display: inline-block !important;
+  padding-left: 20px;
+  padding-top: 2px;
+}
+
+.form-group.remember-me {
+  margin-bottom: 20px;
+  text-align: left;
+}
+
+.btn-register {
+  font-weight: 700;
+  font-size: 2rem;
+  width: 100%;
+  height: 1.8em;
+  margin-top: 1.5rem;
+}
+</style>
